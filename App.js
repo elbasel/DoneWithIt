bb6108cf57ffb7eba6cdd3af585939f1a37bf9e5
elbasel <abdelrahman.elbasel@gmail.com>
2022-09-06 03:49:20 +0200

    icon element finsished

--- App.js
+++ App.js
@@ -1,32 +1,14 @@
-import { StyleSheet, View } from "react-native";
-import ListItemDeleteAction from "./app/components/ListItemDeleteAction";
-
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name="email" size={100} />
+    </Screen>
+  );
 }
-
-// export default function App() {
-//   return (
-//     <View style={styles.containerTwo}>
-//       <ListItemDeleteAction />
-//     </View>
-//   );
-// }
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     // justifyContent: "center",
-//     // alignItems: "center",
-//   },
-//   containerTwo: {
-//     flex: 1,
-//     backgroundColor: "#f8f4f4",
-//     padding: 20,
-//     paddingTop: 100,
-//   },
-// });
