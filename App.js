2a0c8cc3ca4cea84e6618c7617a985b38120fef0
elbasel <abdelrahman.elbasel@gmail.com>
2022-09-14 02:31:14 +0200

    fixing bug - stable build

--- App.js
+++ App.js
@@ -9,57 +9,58 @@ import ListingEditScreen from "./app/screens/ListingEditScreen";
 import RegisterScreen from "./app/screens/RegisterScreen";
 // Screens End
 
-import { createStackNavigator } from "@react-navigation/stack";
+// import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import Screen from "./app/components/Screen";
 import { Button, Text } from "react-native";
 
-const Tweets = ({ navigation }) => {
-  return (
-    <Screen>
-      <Text>Tweets</Text>
-      <Button
-        title="View Tweet Details"
-        onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
-      />
-    </Screen>
-  );
-};
+// const Tweets = ({ navigation }) => {
+//   return (
+//     <Screen>
+//       <Text>Tweets</Text>
+//       <Button
+//         title="View Tweet Details"
+//         onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
+//       />
+//     </Screen>
+//   );
+// };
 
-const TweetDetails = ({ route }) => {
-  const id = route.params.id;
+// const TweetDetails = ({ route }) => {
+//   const id = route.params.id;
 
-  return (
-    <Screen>
-      <Text>Tweet Details {id}</Text>
-      <Link path="Tweets" title="View Tweets" />
-    </Screen>
-  );
-};
+//   return (
+//     <Screen>
+//       <Text>Tweet Details {id}</Text>
+//       <Link path="Tweets" title="View Tweets" />
+//     </Screen>
+//   );
+// };
 
-const Link = ({ title, path }) => {
-  const navigation = useNavigation();
+// const Link = ({ title, path }) => {
+//   const navigation = useNavigation();
 
-  return (
-    <Button title={title || path} onPress={() => navigation.navigate(path)} />
-  );
-};
+//   return (
+//     <Button title={title || path} onPress={() => navigation.navigate(path)} />
+//   );
+// };
 
-const Stack = createStackNavigator();
-const StackNavigator = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Tweets" component={Tweets} />
+// const Stack = createStackNavigator();
+// const StackNavigator = () => {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen name="Tweets" component={Tweets} />
 
-      <Stack.Screen
-        name="TweetDetails"
-        component={TweetDetails}
-        options={({ route }) => ({ title: route.params.id })}
-      />
-    </Stack.Navigator>
-  );
-};
+//       <Stack.Screen
+//         name="TweetDetails"
+//         component={TweetDetails}
+//         options={({ route }) => ({ title: route.params.id })}
+//       />
+//     </Stack.Navigator>
+//   );
+// };
 
 const Tab = createBottomTabNavigator();
 const TabNavigator = () => {
@@ -71,10 +72,18 @@ const TabNavigator = () => {
   );
 };
 
+// j%LbnkvEjgcjhH6
+
 export default function App() {
+  // return (
+  //   <NavigationContainer>
+  //     <TabNavigator />
+  //   </NavigationContainer>
+  // );
+
   return (
-    <NavigationContainer>
-      <TabNavigator />
-    </NavigationContainer>
+    <Screen>
+      <Text>Too slow!?</Text>
+    </Screen>
   );
 }
